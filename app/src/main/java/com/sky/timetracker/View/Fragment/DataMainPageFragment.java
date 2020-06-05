1a07d4de80fd7fcf2358fc86db7fb5d1f34179a6
WEIHAOLEE <1436081191@qq.com>
2020-06-05 17:22:31 +0800

    add indicator Part1

---
+++ app/src/main/java/com/sky/timetracker/View/Fragment/DataMainPageFragment.java
@@ -0,0 +1,47 @@
+package com.sky.timetracker.View.Fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
+
+import com.sky.timetracker.R;
+import com.sky.timetracker.View.adapter.IndicatorAdapter;
+
+import net.lucode.hackware.magicindicator.MagicIndicator;
+import net.lucode.hackware.magicindicator.ViewPagerHelper;
+import net.lucode.hackware.magicindicator.buildins.commonnavigator.CommonNavigator;
+
+public class DataMainPageFragment extends Fragment {
+
+    private View mView;
+    private MagicIndicator mMagicIndicator;
+    private ViewPager mVpData;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        mView = inflater.inflate(R.layout.fragment_main_data_page, container, false);
+        initView();
+        return mView;
+    }
+
+    private void initView() {
+        mMagicIndicator = mView.findViewById(R.id.indicator_data);
+        mMagicIndicator.setBackgroundColor(mView.getResources().getColor(R.color.colorPrimary));
+        // 创建indicator的适配器
+        IndicatorAdapter indicatorAdapter = new IndicatorAdapter();
+        CommonNavigator commonNavigator = new CommonNavigator(mView.getContext());
+        commonNavigator.setAdapter(indicatorAdapter);
+        // ViewPager
+        mVpData = mView.findViewById(R.id.vp_data);
+        // 绑定indicator和viewpager
+        mMagicIndicator.setNavigator(commonNavigator);
+        ViewPagerHelper.bind(mMagicIndicator,mVpData);
+    }
+}
---
+++ app/src/main/java/com/sky/timetracker/View/adapter/IndicatorAdapter.java
@@ -0,0 +1,55 @@
+package com.sky.timetracker.View.adapter;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.view.View;
+
+import com.sky.timetracker.R;
+
+import net.lucode.hackware.magicindicator.buildins.commonnavigator.abs.CommonNavigatorAdapter;
+import net.lucode.hackware.magicindicator.buildins.commonnavigator.abs.IPagerIndicator;
+import net.lucode.hackware.magicindicator.buildins.commonnavigator.abs.IPagerTitleView;
+import net.lucode.hackware.magicindicator.buildins.commonnavigator.indicators.LinePagerIndicator;
+import net.lucode.hackware.magicindicator.buildins.commonnavigator.titles.ColorTransitionPagerTitleView;
+import net.lucode.hackware.magicindicator.buildins.commonnavigator.titles.SimplePagerTitleView;
+
+public class IndicatorAdapter extends CommonNavigatorAdapter {
+
+
+    private final String[] mTitle;
+
+    public IndicatorAdapter(Context context) {
+        mTitle = context.getResources().getStringArray(R.array.indicator_title);
+    }
+
+    @Override
+    public int getCount() {
+        if (mTitle != null) {
+            return mTitle.length;
+        }
+        return 0;
+    }
+
+    @Override
+    public IPagerTitleView getTitleView(Context context, final int index) {
+        SimplePagerTitleView simplePagerTitleView = new ColorTransitionPagerTitleView(context);
+        simplePagerTitleView.setNormalColor(Color.GRAY);
+        simplePagerTitleView.setSelectedColor(Color.WHITE);
+        simplePagerTitleView.setText(mTitle[index]);
+        simplePagerTitleView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO: mViewPager.setCurrentItem(index);
+            }
+        });
+        return simplePagerTitleView;
+    }
+
+    @Override
+    public IPagerIndicator getIndicator(Context context) {
+        LinePagerIndicator linePagerIndicator = new LinePagerIndicator(context);
+        linePagerIndicator.setMode(LinePagerIndicator.MODE_WRAP_CONTENT);
+        linePagerIndicator.setColors(Color.WHITE);
+        return linePagerIndicator;
+    }
+}
