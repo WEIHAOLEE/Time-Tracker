49cfe08f288bb5fade012c921ff65c001367847f
WEIHAOLEE <1436081191@qq.com>
2020-06-07 16:35:48 +0800

    updater chart records for pie chart

--- app/src/main/java/com/sky/timetracker/View/Fragment/DataChartFragment.java
+++ app/src/main/java/com/sky/timetracker/View/Fragment/DataChartFragment.java
@@ -8,6 +8,7 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import com.github.mikephil.charting.charts.PieChart;
 import com.github.mikephil.charting.data.PieData;
@@ -18,6 +19,7 @@ import com.sky.timetracker.Model.ModelImpl;
 import com.sky.timetracker.Presenter.PresenterChartImpl;
 import com.sky.timetracker.R;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class DataChartFragment extends Fragment implements IContract.IViewChart {
@@ -25,6 +27,8 @@ public class DataChartFragment extends Fragment implements IContract.IViewChart 
     private View mView;
     private List<PieEntry> pieEntry;
     private PresenterChartImpl presenterChart;
+    private SwipeRefreshLayout mSwipeRefreshLayout;
+    private PieChart mPieChart;
 
     @Nullable
     @Override
@@ -41,11 +45,35 @@ public class DataChartFragment extends Fragment implements IContract.IViewChart 
     }
 
     private void initView() {
-        PieChart mPieChart = mView.findViewById(R.id.pie_chart);
+        mPieChart = mView.findViewById(R.id.pie_chart);
+        setChart();
+
+
+
+        mSwipeRefreshLayout = mView.findViewById(R.id.refresh);
+        mSwipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                setChart();
+                mSwipeRefreshLayout.setRefreshing(false);
+            }
+        });
+    }
+
+    private void setChart() {
         // 获得数据
         presenterChart.getChartDataList(mView.getContext());
         if (pieEntry != null) {
             PieDataSet set = new PieDataSet(pieEntry, "事件记录");
+            // 颜色
+            ArrayList<Integer> colors = new ArrayList<Integer>();
+            colors.add(getResources().getColor(R.color.colorPrimary));
+            colors.add(getResources().getColor(R.color.colorAccent));
+            colors.add(getResources().getColor(R.color.chartColor1));
+            colors.add(getResources().getColor(R.color.chartColor2));
+            colors.add(getResources().getColor(R.color.chartColor3));
+            colors.add(getResources().getColor(R.color.chartColor4));
+            set.setColors(colors);
             PieData pieData = new PieData(set);
             mPieChart.setData(pieData);
             mPieChart.invalidate();
